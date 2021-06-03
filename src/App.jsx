21e205fc43299a4a930385617aad5d5eac25381a
Agus Maio <agustinmaio93@gmail.com>
2021-06-03 17:28:55 -0300

    app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import logo from './logo-iphonefamily.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          iPhone Family Arg
+        </p>
+        <a
+          className="App-link"
+          href="https://agusmaio.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+        Ingresa!
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
