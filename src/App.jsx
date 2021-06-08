078cb863d6d5ffe58a49c6ef105bcb304d92d547
Agus Maio <agustinmaio93@gmail.com>
2021-06-08 16:45:32 -0300

    component NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
 import logo from './logo-iphonefamily.png';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from './Components/NavBar';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           iPhone Family Arg
         </p>
-        <a
-          className="App-link"
-          href="https://agusmaio.github.io/"
+        <button className="btn btn-primary App-link"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-        Ingresa!
-        </a>
+          rel="noopener noreferrer" 
+          ><a className='link' href="https://agusmaio.github.io/">Ingresa al Sitio</a></button>
       </header>
     </div>
   );
